a04c27419815b5421f308de9e4c5093f0e9e7d8b
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-01-31 07:54:00 +0100

    create first layout components + implementation

---
+++ src/components/Layout/Body.js
@@ -0,0 +1,9 @@
+const Body = (props) => {
+  return ( 
+    <main className="container mx-auto">
+      { props.children }
+    </main>
+  );
+}
+ 
+export default Body;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="px-10 py-5">
+      <nav className="flex w-full justify-between">
+        <div>Food-Order</div>
+        <button className="h-10 w-fit p-3">Cart</button>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
