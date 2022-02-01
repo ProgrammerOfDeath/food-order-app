7ad07e0e4f427d0bea31a769b03a3e04e15534d3
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-02-01 08:27:38 +0100

    adjust header and body - enable dark mode support

--- src/components/Layout/Body.js
+++ src/components/Layout/Body.js
@@ -1,9 +1,9 @@
 const Body = (props) => {
-  return ( 
-    <main className="container mx-auto">
-      { props.children }
+  return (
+    <main className="bg-alice-blue dark:bg-charleston-green text-black dark:text-alice-blue">
+      <div className="container mx-auto pt-28 h-screen">{props.children}</div>
     </main>
-  );
+  )
 }
  
 export default Body;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,11 @@
+import CartButton from "../UI/CartButton"
+
 const Header = () => {
   return (
-    <header className="px-10 py-5">
-      <nav className="flex w-full justify-between">
-        <div>Food-Order</div>
-        <button className="h-10 w-fit p-3">Cart</button>
+    <header className="px-10 py-5 fixed top-0 inset-x-0 shadow-md bg-alice-blue dark:bg-charleston-green text-black dark:text-alice-blue">
+      <nav className="flex w-full justify-between items-center">
+        <div className="text-3xl">Food-Order</div>
+        <CartButton></CartButton>
       </nav>
     </header>
   )
