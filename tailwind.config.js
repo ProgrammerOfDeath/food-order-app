29f731c8f0a2b68fecea4b89b1c846ff46c35833
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-02-03 08:09:50 +0100

    extend theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,13 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   /* darkMode: 'class', */
   theme: {
-    colors: {
-      'charleston-green': '#192224',
-      'alice-blue': '#EDF5FD',
-      'black': '#000000',
+    extend: {
+      colors: {
+        'charleston-green': '#192224',
+        'alice-blue': '#EDF5FD',
+        'black': '#000000',
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
