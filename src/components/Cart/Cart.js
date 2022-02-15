4f1c6f6b0c803c429971f79cc0f4b411ad32b4cf
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-02-15 07:33:28 +0100

    add cart item component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,17 +2,15 @@ import React, { useContext } from "react";
 import CartContext from "../../store/cart-context";
 import Button from "../UI/Button";
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext)
 
   const cartItems = (
-    <ul className="list-none m-0 p-0 overflow-auto max-h-80">
-      {cartCtx.items.map((item, key) => (
-        <li className="flex w-full justify-between" key={key}>
-          <span>{item.name}</span>
-          <span>{item.amount} * {item.price.toFixed(2)}</span>
-        </li>
+    <ul className="list-none m-0 p-0 overflow-y-scroll max-h-80">
+      {cartCtx.items.map((item) => (
+        <CartItem name={item.name} price={item.price} amount={item.amount} id={item.id} />
       ))}
     </ul>
   )
