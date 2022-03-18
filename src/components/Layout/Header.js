5a4d4b12ba3edc7e198faaf601235b81091d7beb
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-03-18 10:31:40 +0100

    test for app hosting

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
     <>
       <header className="px-10 z-10 py-5 fixed top-0 inset-x-0 shadow-md bg-alice-blue dark:bg-charleston-green text-black dark:text-alice-blue">
         <nav className="flex w-full justify-between items-center">
-          <div className="text-3xl">Food-Order</div>
+          <div className="text-3xl">Food-Order v2</div>
           <CartButton onClick={props.onCartClick}></CartButton>
         </nav>
       </header>
